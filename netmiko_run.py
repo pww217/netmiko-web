9607fdde7fd0e6a18024fd546a978d87307adc38
pww217 <pwilson1194@gmail.com>
2021-05-07 13:13:24 -0500

    Cleaned up code some, reformatted readme to be more readable.

--- netmiko_run.py
+++ netmiko_run.py
@@ -1,13 +1,8 @@
 import netmiko
-from getpass import getpass
-import datetime
 import os
+import datetime
 import shutil
-
-"""
-Add error handling for missing/empty host/config files.
-
-"""
+from getpass import getpass
 
 def ExtractHosts(): # Extract list of hosts from file
     with open('hosts.txt', 'r') as hostfile:
@@ -35,13 +30,14 @@ for device in hosts:
     print(device.rstrip('\n'))
 print()
 
-# If/else blocks collect information of command source and privileges
+# If/else loops collect information of command source and privileges
+
 while True:
-    ad_hoc_or_file = input('Are you making config changes from a file? (y/n): ')
-    if ad_hoc_or_file not in ('y', 'n'):
+    ad_hoc_or_input_file = input('Are you making config changes from a file? (y/n): ')
+    if ad_hoc_or_input_file not in ('y', 'n'):
         print('Not a valid response\n')
         continue
-    if ad_hoc_or_file == 'y':
+    if ad_hoc_or_input_file == 'y':
         warning = input('\nPlease note - this will make config changes to \n\
 the live running config, which can be VERY risky. \n\
 Please write memory first. Are you sure you want to proceed? (y/n): ')
@@ -52,8 +48,9 @@ Please write memory first. Are you sure you want to proceed? (y/n): ')
             exit()
     else:
         break
-# Asks about enable mode
-if ad_hoc_or_file == 'n':
+
+# Asks about enable mode privileges
+if ad_hoc_or_input_file == 'n':
     while True:
         enable_required = input('Does this command require enable mode? y/n : ')
         if enable_required not in ('y', 'n'):
@@ -79,12 +76,12 @@ for device in hosts:
         connection = netmiko.ConnectHandler(host=device, device_type='cisco_xe', username=username, password=password, secret=password)
         if enable_required.lower() == 'y': # Enters enable mode if requested
             connection.enable()
-        if ad_hoc_or_file.lower() == 'n':
+        if ad_hoc_or_input_file.lower() == 'n':
             reply = f'{device} returned the following output:\n\n' \
             + connection.send_command(command) # Runs ad-hoc command
             print(reply)
             WriteToFile(reply)
-        if ad_hoc_or_file.lower() == 'y':
+        if ad_hoc_or_input_file.lower() == 'y':
             reply = f'{device} returned the following output:\n\n' \
             + connection.send_config_from_file(command_file) # Pulls config from file and runs
             print(reply)
@@ -92,7 +89,7 @@ for device in hosts:
         connection.disconnect()
         print()
 
-    # These two except clauses handle common errors
+    # These except clauses handle common errors
     except netmiko.ssh_exception.NetmikoTimeoutException:
         print(f'{device} could not be reached on port 22 - skipping to next device.\n')
         with open(outfile, 'a') as f:
@@ -106,23 +103,26 @@ for device in hosts:
     except netmiko.ssh_exception.NetmikoAuthenticationException:
         print('SSH Credentials are not valid - please check and try again.')
         exit()
+
 # Create logdir if not existing already
 if os.path.isdir('logs') == False:
     os.mkdir(cwd+'\\logs')
 logdir = cwd+'\\logs\\'
+
 # Remove output.txt if program failed to finish last time
 if os.path.exists(logdir+'output.txt'):
     os.remove(logdir+'output.txt')
+
 # Two kinds of files are written afterwards based on command type
-if ad_hoc_or_file == 'n':
+if ad_hoc_or_input_file == 'n':
     shutil.move(outfile, logdir)
     os.chdir(logdir)
-    # Name after command and remove illegal characters from file name
+    # Name file after running command and remove illegal characters from file name
     new_file_name = command.replace('|', '')
     new_file_name = new_file_name.replace(' ', '_')
     print('New file created in logs/' + new_file_name + f'_{date.month}-{date.day}-{date.year}-{date.hour}-{date.minute}.txt')
     os.rename(outfile, new_file_name + f'_{date.month}-{date.day}-{date.year}-{date.hour}-{date.minute}.txt')
-if ad_hoc_or_file == 'y':
+if ad_hoc_or_input_file == 'y':
     shutil.move(outfile, logdir)
     os.chdir(logdir)
     os.rename(outfile, f'config_changes_{date.month}-{date.day}-{date.year}-{date.hour}-{date.minute}.txt')
--- readme.md
+++ readme.md
@@ -1,10 +1,24 @@
-This program will allow you use run ad-hoc commands in user or enable mode on a Cisco IOS-XE device, as well as submit configuration changes from a file directly.
+-- Netmiko Run ---  pww217, 2021
 
-How to use:
+This program will allow you use run ad-hoc commands in user or enable mode on a Cisco 
+IOS-XE device, as well as submit configuration changes from an input file directly.
+
+--- How to Use ---
 1. From Windows cmd line, powershell, or another shell, change directory (cd) until you reach the program's directory containing .py and dependent files
 2. Edit host and cmd files to your liking, one command or host per line. Use template files as guides.
-3. Run "python netmiko_run" or "python3 netmiko_run" depending on your environmental variables
-4. At the first prompt, select y if making config-mode changes from the file config_commands.txt. Note: 'conf t' and 'exit' do not need to be written in config_commands.txt - these are implied. Otherwise choose n to run a single, ad-hoc command.
-5. (If selected ad-hoc) specify whether enable mode is necessary
-6. (If selected ad-hoc) write the command you want to run. Pipes '|' and other advanced operators are allowed.
-7. Output will be written to shell as well as a file with a timestamp to /logs in the working directory
+3. Run "python netmiko_run" or "python3 netmiko_run" depending on your python version and environmental variables
+4. Choose one of the below modes:
+
+--- Configuration Mode - merge commands from a file into running configuration ---
+
+1. At the first prompt, select 'y' if making config-mode changes from the file config_commands.txt. Otherwise choose n to run a single, ad-hoc command.
+   Note: 'conf t' and 'exit' do not need to be written in config_commands.txt - these are implied. 
+2. Enter device credentials when prompted. These can be local, TACACS, ISE - anything the device will authenticate.
+3. Output will be written to shell as well as a file with a timestamp to /logs in the working directory
+
+--- Ad Hoc Mode - run a single command in user or enable mode ---
+
+1. Specify whether enable mode is necessary
+2. Write the command you want to run. Pipes '|' and other advanced operators are supported.
+3. Enter device credentials when prompted. These can be local, TACACS, ISE - anything the device will authenticate.
+4. Output will be written to shell as well as a file with a timestamp to /logs in the working directory
