09a64d06fb259503733b82dd1c47478f0f75bff9
Peter <pww217@gmail.com>
2021-08-15 13:09:21 -0500

    added logging, logfile, logic cleanup

--- netmiko_run.py
+++ netmiko_run.py
@@ -3,15 +3,23 @@ import os
 import datetime
 import shutil
 from getpass import getpass
+import logging
+
+# Configure logging
+logging.basicConfig(filename='run.log', level=logging.INFO)
+
+# Function defs
 
 def ExtractHosts(): # Extract list of hosts from file
     with open('hosts.txt', 'r') as hostfile:
         hosts = hostfile.readlines()
+    logging.debug(f'Hosts are: {hosts}')
     return hosts
 
 def ExtractCommands(): #
     with open('config_commands.txt', 'r') as cmdfile:
         commands = cmdfile.readlines()
+    logging.debug(f'Found commands are: {commands}')
     return commands
 
 def WriteToFile(reply):
@@ -30,17 +38,21 @@ for device in hosts:
     print(device.rstrip('\n'))
 print()
 
-# If/else loops collect information of command source and privileges
+"""
+Flow Logic
 
+"""
+
+# If/else loops collect information of command source and privileges
 while True:
-    ad_hoc_or_input_file = input('Are you making config changes from a file? (y/n): ')
-    if ad_hoc_or_input_file not in ('y', 'n'):
+    use_input_file = input('Are you making config changes from a file? (y/n): ').lower()
+    if use_input_file not in ('y', 'n'):
         print('Not a valid response\n')
         continue
-    if ad_hoc_or_input_file == 'y':
+    elif use_input_file == 'y':
         warning = input('\nPlease note - this will make config changes to \n\
 the live running config, which can be VERY risky. \n\
-Please write memory first. Are you sure you want to proceed? (y/n): ')
+Please write memory first. Are you sure you want to proceed? (y/n): ').lower()
         if warning == 'y':
             break
         else:
@@ -50,9 +62,9 @@ Please write memory first. Are you sure you want to proceed? (y/n): ')
         break
 
 # Asks about enable mode privileges
-if ad_hoc_or_input_file == 'n':
+if use_input_file == 'n':
     while True:
-        enable_required = input('Does this command require enable mode? y/n : ')
+        enable_required = input('Does this command require enable mode? (y/n) : ').lower()
         if enable_required not in ('y', 'n'):
             print('Not a valid response\n')
             continue
@@ -62,6 +74,11 @@ if ad_hoc_or_input_file == 'n':
 else:
      enable_required = 'y'
 print()
+
+"""
+Main program begins
+
+"""
 
 # Request SSH credentials
 username = input('Please enter an SSH username: ')
@@ -74,19 +91,19 @@ for device in hosts:
         device = device.rstrip('\n')
         # Below line forms the SSH connection with supplied variables
         connection = netmiko.ConnectHandler(host=device, device_type='cisco_xe', username=username, password=password, secret=password)
-        if enable_required.lower() == 'y': # Enters enable mode if requested
+        if enable_required == 'y': # Enters enable mode if requested
             connection.enable()
-        if ad_hoc_or_input_file.lower() == 'n':
+        if use_input_file == 'n':
             reply = f'{device} returned the following output:\n\n' \
             + connection.send_command(command) # Runs ad-hoc command
             print(reply)
             WriteToFile(reply)
-        if ad_hoc_or_input_file.lower() == 'y':
+        if use_input_file == 'y':
             reply = f'{device} returned the following output:\n\n' \
             + connection.send_config_from_file(command_file) # Pulls config from file and runs
             print(reply)
             WriteToFile(reply)
-        connection.disconnect()
+        connection.disconnect() # Graceful SSH terminate
         print()
 
     # These except clauses handle common errors
@@ -106,15 +123,15 @@ for device in hosts:
 
 # Create logdir if not existing already
 if os.path.isdir('logs') == False:
-    os.mkdir(cwd+'\\logs')
+    os.mkdir(cwd+'\\logs') # Only works on Windows for now!
 logdir = cwd+'\\logs\\'
 
-# Remove output.txt if program failed to finish last time
+# Clean up output.txt if program failed to finish last time
 if os.path.exists(logdir+'output.txt'):
     os.remove(logdir+'output.txt')
 
-# Two kinds of files are written afterwards based on command type
-if ad_hoc_or_input_file == 'n':
+# Two kinds of log files are written afterwards based on command type
+if use_input_file == 'n':
     shutil.move(outfile, logdir)
     os.chdir(logdir)
     # Name file after running command and remove illegal characters from file name
@@ -122,7 +139,7 @@ if ad_hoc_or_input_file == 'n':
     new_file_name = new_file_name.replace(' ', '_')
     print('New file created in logs/' + new_file_name + f'_{date.month}-{date.day}-{date.year}-{date.hour}-{date.minute}.txt')
     os.rename(outfile, new_file_name + f'_{date.month}-{date.day}-{date.year}-{date.hour}-{date.minute}.txt')
-if ad_hoc_or_input_file == 'y':
+else:
     shutil.move(outfile, logdir)
     os.chdir(logdir)
     os.rename(outfile, f'config_changes_{date.month}-{date.day}-{date.year}-{date.hour}-{date.minute}.txt')
